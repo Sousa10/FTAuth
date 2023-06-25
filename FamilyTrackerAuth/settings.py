5eaa16e3ff2a12f387089424c8e045b49f65e6de
Kirk Shelton <kirk.shelton@tracker-css.com>
2023-06-25 19:58:20 -0400

    Changed redirect URLs for login and logout

--- FamilyTrackerAuth/settings.py
+++ FamilyTrackerAuth/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 LOGIN_URL = '/LoginRegister/login/'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/LoginRegister/FTMainMenu'
+LOGOUT_REDIRECT_URL = '/LoginRegister/login/'
 
 
 # Application definition
