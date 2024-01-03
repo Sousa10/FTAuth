e4c143eb1f3ca945888a922fc8486eeb645ff0e9
Kirk Shelton <kirk.shelton@tracker-css.com>
2024-01-03 18:45:11 -0500

    Kirk Calendar Rui Clicks counter by app

--- calendar10/urls.py
+++ calendar10/urls.py
@@ -6,4 +6,8 @@ app_name = 'calendar10'
 urlpatterns = [
     path('', views.home, name="home"),
     path('main_landing_page/', views.main_landing_page, name='main_landing_page'),
+    path('event/new/', views.event, name='event_new'),
+    path('event/edit/<int:event_id>/', views.event, name='event_edit'),
+    path('calendar/', views.CalendarView.as_view(), name='calendar'),
+    # path('event/', views.event, name='event_new'),
 ]
