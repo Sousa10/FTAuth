3daae46cb3513d5171020ec3a9a53d91e005d7ce
Rui Sousa <ruisousa05@gmail.com>
2024-05-04 22:12:31 -0100

    added spaces for static files

--- FamilyTrackerAuth/settings.py
+++ FamilyTrackerAuth/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'listsplan',
     'administration',
     'django_bootstrap_icons',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -174,6 +175,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+from .cdn.conf import * #noqa
+
+# https://traxx.nyc3.digitaloceanspaces.com
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
