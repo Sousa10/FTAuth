1074c413a94fde67e6d247cf5a317c8fa4b5285e
Rui Sousa <ruisousa05@gmail.com>
2024-05-13 21:56:54 -0100

    added login required to main menu

--- LoginRegister/views.py
+++ LoginRegister/views.py
@@ -1,15 +1,12 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.template import loader
 from .models import PersonM, DefaultParams, SponRates
 from .forms import SponRatesForm
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
-from django.contrib import messages
-from datetime import datetime
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import logout
-import os
-from listsplan.models import ListHeaderT
+from django.contrib.auth.decorators import login_required
 
 def logout_user(request):
     logout(request)
@@ -23,6 +20,7 @@ def LoginRegister(request):
     }
     return HttpResponse(template.render(context, request))
 
+@login_required
 def FTMainMenu(request):
     template = loader.get_template('LoginRegister/FTMainMenu.html')
     return HttpResponse(template.render())
